d19066e963d3277faac357a0c2dffba605ff1cb3
kzcyanide <snjt700@gmail.com>
2024-03-25 18:46:14 +0530

    Added Missing Value section and EDA (todo) later

--- app.py
+++ app.py
@@ -38,19 +38,69 @@ if st.session_state['df'] is not None:
 
     # EDA
 
-    st.markdown('### Exploratory Data Analysis')
+    st.markdown('### 📊 Exploratory Data Analysis', unsafe_allow_html=True)
     nLine()
-    with st.expander("EDA"):
+
+    with st.expander("Basic Information"):
         nLine()
 
         head = st.checkbox('Show 5 Samples ',value = False)
         if head:
             nLine()
             st.dataframe(df.sample(5), use_container_width=True)
+        
         shape = st.checkbox('Show Shape',value=False)
         if shape:
-            st.write(f'This Dataframe has **{df.shape[0]} rows** and **{df.shape[1]} columns**')    
+            st.write(f'This Dataframe has **{df.shape[0]} rows** and **{df.shape[1]} columns**')
 
-   
+        numStats = st.checkbox('Discriptive Statistics **(Numerical)**')
+        if numStats:
+            nLine()
+            st.dataframe(round(df.describe(),2),use_container_width=True)
+
+        catStats = st.checkbox('Discriptive Statistics **(Categorical)**')
+        if catStats:
+            if df.select_dtypes(include=object).columns.tolist():
+                nLine()
+                st.dataframe(df.describe(include=['object']),use_container_width=True)
+            else:
+                st.write('This Dataframe has no **Categorical** Features')    
+
+        missing = st.checkbox('Show Missing Values',value=False)
+        if missing:
+
+            st.markdown("<h6 align='center'> Null Values",unsafe_allow_html=True)
+            st.dataframe(df.isnull().sum().sort_values(ascending=False),height=350, use_container_width=True)
+
+        #TODO Duplicated values
+
+
+    with st.expander("Show EDA"):
+        st.write('TODO')
+
+    nLine()
+
+    # Missing Values
+
+    st.markdown('### ⚠️ Handling Missing Values', unsafe_allow_html=True)
+    nLine()
+
+    with st.expander('Show Missing Values'):
+        
+        nLine()
+        # Input box
+        col1,col2 = st.columns(2)
+        with col1:
+            missingCols = df.columns[df.isnull().any()].to_list()
+            if missingCols:
+                opts = ['All Numerical Features','All Categorical Features']
+
+            else:
+                opts = []
+
+            fillFeatures = st.multiselect('Select Features', missingCols + opts, help='Select Features')
+
+        with col2:
+            strategy = st.selectbox('Select Strategy For Filling',['Select', 'Drop Rows', 'Drop Columns', 'Fill with Mean', 'Fill with Median','Fill with Mode (Most Frequent)','Fill with ffill, bfill'], help='Select Missing Values Strategy')
 
 
